b4c229a198c5a87140b8d8a4f5457efc1cb6ac93
sullagzh <23520599@gm.uit.edu.vn>
2024-11-03 21:45:51 +0700

    Cập nhật một số thông tin sẽ hiện lên giao diện

--- CaRo_Game/Client/PlayInfo.cs
+++ CaRo_Game/Client/PlayInfo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace Client
 {
@@ -10,13 +11,26 @@ namespace Client
     {
         private Point point;
         private int currentPlayer;
+
+        // Thêm các thuộc tính mới
+        public string Username { get; set; }
+        public string Avatar { get; set; }
+        public int GamesPlayed { get; set; }
+        public int Wins { get; set; }
+        public int Rank { get; set; }
         public Point Point { get => point; set => point=value; }
         public int CurrentPlayer { get => currentPlayer; set => currentPlayer=value; }
 
-        public PlayInfo(Point point, int currentPlayer)
+        // Constructor với các thông tin bổ sung
+        public PlayInfo(string username, string avatar, Point point, int currentPlayer)
         {
+            this.Username = username;
+            this.Avatar = avatar;
             this.Point = point;
             this.CurrentPlayer = currentPlayer;
+            this.GamesPlayed = 0; // Khởi tạo số ván đã chơi
+            this.Wins = 0;        // Khởi tạo số trận thắng
+            this.Rank = 0;       // Khởi tạo thứ hạng
         }
     }
 }
