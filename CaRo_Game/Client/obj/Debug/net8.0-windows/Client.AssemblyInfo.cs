8b572b2e9f279e33cbdcf31bb83048b7ab019664
Nguyễn Minh Quyền <23521325@gm.uit.edu.vn>
2024-12-30 21:41:45 +0700

    nothing

--- CaRo_Game/Client/obj/Debug/net8.0-windows/Client.AssemblyInfo.cs
+++ CaRo_Game/Client/obj/Debug/net8.0-windows/Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e7cdfdd95a781ebede16ca5cd54b9e3c25f319e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff579a48b18c20469dbbe8d44b7f66e3e5f6ae17")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
